dd09a700c6368b03053f33d20170a0be795d7943
Svitlana Chudesa <chsveta34@gmail.com>
2022-06-10 19:50:35 +0300

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import Player from '@vimeo/player';
+
 import throttle from 'lodash.throttle';
 
 
-const KEY_VIDEOPLAYER = "videoplayer-current-time";
+const VIDEOPLAYER_KEY = "videoplayer-current-time";
  
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
@@ -12,8 +12,8 @@ const player = new Vimeo.Player(iframe);
  player.on('timeupdate', throttle(setPlayerTimer, 1000));
 
  function setPlayerTimer(e) {
-  localStorage.setItem(KEY_VIDEOPLAYER, e.seconds);
+  localStorage.setItem(VIDEOPLAYER_KEY, e.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem(KEY_VIDEOPLAYER));
+player.setCurrentTime(localStorage.getItem(VIDEOPLAYER_KEY));
 
